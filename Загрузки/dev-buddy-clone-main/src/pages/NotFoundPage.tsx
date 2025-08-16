ccf1a53896dc4c2d157553f98dd2540400ae3548
Mikhail Singatullin <msingatullin@gmail.com>
2025-08-17 00:54:45 +0300

    Добавлена настройка TailwindCSS и PostCSS

--- Загрузки/dev-buddy-clone-main/src/pages/NotFoundPage.tsx
+++ Загрузки/dev-buddy-clone-main/src/pages/NotFoundPage.tsx
@@ -6,6 +6,7 @@ function NotFoundPage() {
     <div className="container mx-auto p-4 text-center">
       <h1 className="text-4xl font-bold">404</h1>
       <p className="mt-4 text-xl">Страница не найдена</p>
+      <p className="mt-2 text-green-500 font-medium">TailwindCSS настроен успешно!</p>
       <Link to="/" className="mt-4 inline-block text-blue-500 hover:underline">
         Вернуться на главную
       </Link>
